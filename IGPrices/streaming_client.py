a9764322c364ca6b6548ff1e8ee3e26a1b206fe2
Darren Lawton <darren@darrenlawton.com>
2020-08-30 15:53:48 +1000

    streaming client done.

--- IGPrices/streaming_client.py
+++ IGPrices/streaming_client.py
@@ -12,17 +12,23 @@ class IG_streaming_session(IGSession, LSClient):
     def __init__(self, api_key: str, ulogin_details: dict):
         IGSession.__init__(self, api_key, ulogin_details)
         if self.login():
-            self.connect_LSClient()
+            self.connect_lsclient()
 
-    def connect_LSClient(self):
-        LSusername = self.login_details['username']
-        LSpassword = f"CST-{self.authorisation_headers['CST']}|XST-{self.authorisation_headers['X-SECURITY-TOKEN']}"
-        LSendpoint = self.lightstreamerEndpoint
-        logger.info("Starting connection with %s" % LSendpoint)
-        LSClient.__init__(self, LSendpoint, user=LSusername, password=LSpassword)
+    def connect_lsclient(self):
+        ls_username = self.login_details['username']
+        ls_password = f"CST-{self.authorisation_headers['CST']}|XST-{self.authorisation_headers['X-SECURITY-TOKEN']}"
+        ls_endpoint = self.lightstreamerEndpoint
+        logger.info("Starting connection with %s" % ls_endpoint)
+        LSClient.__init__(self, ls_endpoint, user=ls_username, password=ls_password)
         try:
             self.connect()
-        except Exception:
+        except Exception as e:
             logger.error("Unable to connect to Lightstreamer Server")
             logger.error(traceback.format_exc())
-            sys.exit(1)
+            raise e
+
+    def disconnect(self):
+        # disconnect from LS server
+        self.disconnect()
+        # log out of IG session
+        self.logout()
